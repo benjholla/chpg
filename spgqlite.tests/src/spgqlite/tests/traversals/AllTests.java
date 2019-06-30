51c5a1e6f3353c755f50ebbf68efcfe64b1cbfcf
Benjamin Holland <benjholla@gmail.com>
2019-06-30 11:51:13 -0400

    developing a test suite

---
+++ spgqlite.tests/src/spgqlite/tests/traversals/AllTests.java
@@ -0,0 +1,11 @@
+package spgqlite.tests.traversals;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestGraphForwardTraversals.class })
+public class AllTests {
+
+}
